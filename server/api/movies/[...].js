cde7932b66e2b905b5bb4395e5d34f2b070ee698
Rafael Alves <rafalves.costa@gmail.com>
2023-10-07 22:36:53 -0300

    fix base api url

--- server/api/movies/[...].js
+++ server/api/movies/[...].js
@@ -1,6 +1,7 @@
 export default defineEventHandler((event) => {
   const id = [...event.node.req.url.split("/")].pop();
   const config = useRuntimeConfig();
+  console.log('api access token: ' + apiAccessToken)
   return $fetch(`${config.public.apiBaseUrl}/movie/${id}`, {
     method: "GET",
     headers: {
--- server/api/movies/search.js
+++ server/api/movies/search.js
@@ -1,6 +1,7 @@
 export default defineEventHandler((event) => {
     const  { query, page } = getQuery(event);
-    const config  = useRuntimeConfig();
+    const config = useRuntimeConfig();
+    console.log('api access token: ' + apiAccessToken)
     return $fetch(`${config.public.apiBaseUrl}/search/movie?query=${query}&page=${page}`, {
         method: "GET",
         headers: {
