2036ac747312326e8c9a0152b2f88049ff24be18
Rafael Alves <rafalves.costa@gmail.com>
2023-10-07 22:47:35 -0300

    fix base api url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
     "@nuxt/devtools"
   ],
   runtimeConfig: {
-    apiAccessToken: "",
+    apiAccessToken: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNDNiODI2YTY1Zjc5NjEzYzRmYjIzZTJlMDdlNmYwZiIsInN1YiI6IjYyOGEyZDZhMjQ5NWFiNTI3ZGQwYWU2ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qINsKnzOaTLkUGn0Q0vYjCpmM4XWqQm44gKG4PouDkg",
     public: {
       apiBaseUrl: "https://api.themoviedb.org/3",
       imgBaseUrl: "https://image.tmdb.org/t/p/w500",
